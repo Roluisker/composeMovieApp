da58ef74706fe6daddf2236fe8d15e47a842ef1c
Luis Bejarano <roluisker@gmail.com>
2025-07-15 13:10:20 -0500

    bottom navigation

--- app/src/main/java/com/example/composemovieapp/MainActivity.kt
+++ app/src/main/java/com/example/composemovieapp/MainActivity.kt
@@ -6,11 +6,29 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.PlayArrow
+import androidx.compose.material.icons.outlined.FavoriteBorder
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.outlined.PlayArrow
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
+import com.example.composemovieapp.navigation.PrincipalScreenItem
+import com.example.composemovieapp.navigation.Routes
 import com.example.composemovieapp.presentation.detail.DetailsScreen
 import com.example.composemovieapp.presentation.home.MoviesListScreen
 import com.example.composemovieapp.ui.theme.ComposeMovieAppTheme
@@ -23,19 +41,34 @@ class MainActivity : ComponentActivity() {
         setContent {
             ComposeMovieAppTheme {
                 val navController = rememberNavController()
+                val backStackEntry by navController.currentBackStackEntryAsState()
+                val currentDestination = backStackEntry?.destination
 
-                Scaffold { paddingValues ->
+                Scaffold(
+                    bottomBar = {
+                        PrincipalNavigationBar(
+                            navController = navController,
+                            currentDestination = currentDestination
+                        )
+                    }
+                ) { paddingValues ->
                     NavHost(
                         modifier = Modifier.padding(paddingValues),
                         navController = navController,
-                        startDestination = "movieList"
+                        startDestination = Routes.HomeSreen
                     ) {
-                        composable(route = "movieList") {
+                        composable(route = Routes.HomeSreen) {
                             MoviesListScreen(onMovieClick = { movieModel ->
-                                navController.navigate("details")
+                                navController.navigate(Routes.Detailsscreen)
                             })
                         }
-                        composable(route = "details") {
+                        composable(route = Routes.FavoritiesScreen) {
+
+                        }
+                        composable(route = Routes.NowPLayingScreen) {
+
+                        }
+                        composable(route = Routes.Detailsscreen) {
                             DetailsScreen()
                         }
                     }
@@ -43,6 +76,54 @@ class MainActivity : ComponentActivity() {
 
 
             }
+        }
+    }
+}
+
+@Composable
+fun PrincipalNavigationBar(
+    navController: NavController,
+    currentDestination: NavDestination?
+) {
+    val principalScreens = listOf(
+        PrincipalScreenItem(
+            title = "Home",
+            route = Routes.HomeSreen,
+            selectedIcon = Icons.Filled.Home,
+            unselectedIcon = Icons.Outlined.Home
+        ),
+        PrincipalScreenItem(
+            title = "Favorities",
+            route = Routes.FavoritiesScreen,
+            selectedIcon = Icons.Filled.Favorite,
+            unselectedIcon = Icons.Outlined.FavoriteBorder
+        ),
+        PrincipalScreenItem(
+            title = "Now Playing",
+            route = Routes.NowPLayingScreen,
+            selectedIcon = Icons.Filled.PlayArrow,
+            unselectedIcon = Icons.Outlined.PlayArrow
+        )
+    )
+
+    NavigationBar {
+        principalScreens.forEach { screen ->
+            NavigationBarItem(
+                label = {
+                    Text(text = screen.title)
+                },
+                selected = currentDestination?.route == screen.route,
+                onClick = {
+                    navController.navigate(screen.route)
+                },
+                icon = {
+                    Icon(
+                        imageVector = if (currentDestination?.route == screen.route) screen.selectedIcon else screen.unselectedIcon,
+                        contentDescription = screen.title
+                    )
+                }
+            )
+
         }
     }
 }
---
+++ app/src/main/java/com/example/composemovieapp/navigation/Routes.kt
@@ -0,0 +1,8 @@
+package com.example.composemovieapp.navigation
+
+object Routes {
+    val HomeSreen = "home"
+    val Detailsscreen = "details"
+    val FavoritiesScreen = "favs"
+    val NowPLayingScreen = "nowplaying"
+}
